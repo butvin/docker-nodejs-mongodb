f70280c98616b2c24016bab5d734399ef0cb006a
butvin <butvin.sergey@gmail.com>
2021-07-10 03:54:05 +0300

    v0.0.2-fix

--- node-server.js
+++ node-server.js
@@ -1,4 +1,31 @@
-const express = require('express');
-const mongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
-const app = express();
+// include package express and  app instance
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+
+
+const app = express()
+console.log(app)
+
+// mongodb checking
+const url = 'mongodb://mongodb:27017'
+
+// const url = process.env.MONGO_CONNECT_URL
+MongoClient.connect(url, (error, client) => {
+    if(error){
+        throw new Error(`No connect to the database`)
+    }
+
+    console.log(`Successfully connected to the database`)
+})
+
+const port = process.env.APP_PORT || 3000
+app.listen(3000, () => {
+    console.log('Express is listening on port 3000!')
+})
+
+app.get('/', (req, res) => {
+    res.send('Index page');
+    console.log(req)
+    console.log(res)
+});
+
--- src/index.js
+++ src/index.js
@@ -5,26 +5,9 @@ console.log(express)
 const app = express()
 console.log(app)
 
-app.listen(3000, () => {
-    console.log('Express is listening on port 3000!')
-})
+const bodyParser = require('body-parser')
 
-const MongoClient = require('mongodb').MongoClient;
 
-const url = 'mongodb://mongodb:27017'
-// const url = process.env.MONGO_CONNECT_URL
 
-MongoClient.connect(url, (error, client) => {
-    if(error){
-        throw new Error(`No connect to the database`)
-    }
 
-    console.log(`Successfully connected to the database`)
-})
-
-app.get('/', (req, res) => {
-    res.send('Index page');
-    console.log(req)
-    console.log(res)
-});
 
